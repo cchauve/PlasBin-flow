f863e818159ec59f4983abe46fd5217bce5037db
Cedric Chauve <chauvec@cedar1.cedar.computecanada.ca>
2023-08-24 10:19:36 -0700

    Improved GFA attributes reading

--- code/gfa_fasta_utils.py
+++ code/gfa_fasta_utils.py
@@ -171,7 +171,7 @@ GFA_TO_ORIENT_KEY = 'ToOrient'
 GFA_OVERLAP_KEY = 'Overlap'
 
 # Conversionto of GFA attributes.
-# Missing attributes: B, J
+# Missing attributes types: B, J
 GFA_ATTRIBUTE_TYPE = {
     'i': lambda x: int(x),
     'f': lambda x: float(x),
@@ -187,16 +187,60 @@ def __add_attributes(attributes_data, attributes_list):
     Args:
         - attributes_data (List): list of attributes in format str(key:type:value)
         - attributes_list (List(str)): list of attribute keys to read
+          ['all'] for recording all attributes 
 
     Returns:
-       - (Dictionary) attribute key: attribute value (None if missing attribute)
+        - (Dictionary) attribute key: attribute value (None if missing attribute)
+        attributes values are converted to the type defined by GFA_ATTRIBUTE_TYPE
+        if an attribute key in attributes_list is not a key in GFA_ATTRIBUTE_TYPE
+        the attribute value is recorded as a string
     """
-    attributes_dict = {att_key: None for att_key in attributes_list}
+    attributes_dict = {
+        att_key: None
+        for att_key in attributes_list
+        if attributes_list != ['all']
+    }
     for att_data in attributes_data:
         att_key,att_type,att_val = att_data.split(':')
-        if att_key in attributes_list:
-            attributes_dict[att_key] = GFA_ATTRIBUTE_TYPE[att_type](att_val)
+        if attributes_list == ['all'] or att_key in attributes_list:
+            if att_type not in GFA_ATTRIBUTE_TYPE.keys():
+                attributes_dict[att_key] = att_val
+            else:
+                attributes_dict[att_key] = GFA_ATTRIBUTE_TYPE[att_type](att_val)
     return attributes_dict
+
+def __write_attributes(attributes_dict, keys_to_remove=[], sep=' '):
+    """
+    Write GFA attributes into a string
+
+    Args:
+        - attributes_dict (Dictionary): attribute key -> attribute value
+        - keys_to_remove (List(str)): list of keys to not print
+        - sep (str): separating string
+
+    Returns:
+        (str): list of attributes in format sep.join(key:value)
+        attributes with None value are not written
+    """
+    return sep.join(
+        [
+            f'{x}:{y}'
+            for x,y in attributes_dict.items()
+            if x not in keys_to_remove and y is not None
+        ]
+    )
+
+def __assert_attributes_list(attributes_list):
+    """
+    Assert that an attributes list is either ['all'] or does not contain 'all'
+    Used only for development
+
+    Args:
+        - attributes_list (List(str))
+    """
+    assert (
+        attributes_list == ['all'] or 'all' not in attributes_list
+    ), f'incorrect GFA attributes list {attributes_list}'
 
 def read_GFA_ctgs(in_file_path, attributes_list, gzipped=False):
     """
@@ -205,6 +249,7 @@ def read_GFA_ctgs(in_file_path, attributes_list, gzipped=False):
     Args:
         - in_file_path (str): path to GFA file to read
         - attributes_list (List(str)): list of attribute keys to read
+          ['all'] for recording all attributes
         - gzipped (bool): True if gzipped GFA file
 
     Returns:
@@ -213,6 +258,7 @@ def read_GFA_ctgs(in_file_path, attributes_list, gzipped=False):
     Assumption: 
        - every contig has an associated id and sequence (not checked)
     """
+    __assert_attributes_list(attributes_list)
     result = {}
     with __open_file_read(in_file_path, gzipped) as in_file:
         for line in [x for x in in_file.readlines() if x[0]=='S']:
@@ -238,10 +284,8 @@ def read_GFA_links(in_file_path, attributes_list, gzipped=False):
          List(links from contig id
            (Dictionary) attribute key: attribute value (None if missing attribute))
            graph attributes: GFA_FROM_ORIENT_KEY, GFA_TO_KEY, GFA_TO_ORIENT_KEY, GFA_OVERLAP_KEY
-
-    Assumption: 
-       - every contig has an associated id and sequence (not checked)
     """
+    __assert_attributes_list(attributes_list)
     result = defaultidct(list)
     with __open_file_read(in_file_path, gzipped) as in_file:
         for line in [x for x in in_file.readlines() if x[0]=='L']:
@@ -252,23 +296,46 @@ def read_GFA_links(in_file_path, attributes_list, gzipped=False):
             ctg_to_orient = ctg_data[4]
             overlap = ctg_data[5]
             result[ctg_from].append(
-                __add_attributes([
-                    f'{GFA_TO_KEY}:Z:{ctg_to}',
-                    f'{GFA_FROM_ORIENT_KEY}:A:{ctg_from_orient}',
-                    f'{GFA_TO_ORIENT_KEY}:A:{ctg_to_orient}',
-                    f'{GFA_OVERLAP_KEY}:Z:{overlap}'
-                ] + ctg_data[6:], attributes_list)
+                __add_attributes(
+                    [
+                        f'{GFA_TO_KEY}:Z:{ctg_to}',
+                        f'{GFA_FROM_ORIENT_KEY}:A:{ctg_from_orient}',
+                        f'{GFA_TO_ORIENT_KEY}:A:{ctg_to_orient}',
+                        f'{GFA_OVERLAP_KEY}:Z:{overlap}'
+                    ] + ctg_data[6:],
+                    attributes_list
+                )
             )
     return result
 
-def write_GFA_to_FASTA(in_GFA_file, out_FASTA_file, in_gzipped, out_gzipped):
+def write_GFA_to_FASTA(in_GFA_file, out_FASTA_file, in_gzipped, out_gzipped, sep=' '):
+    """
+    Create a FASTA file from a GFA file
+
+    Args:
+        - in_GFA_file (str): path to GFA file to read
+        - out_FASTA_file (str): path to FASTA file to write
+        - in_gzipped (bool): True if gzipped GFA file
+        - out_gzipped (bool): True if FASTA file to be gzipped
+        - sep (str): string for separating GFA attributes
+
+    Returns:
+        None, creates file out_FASTA_file
+        header format: <contig name> <contig name>.GFA <attributes string=sep.join(key:value)>
+        
+    """    
     GFA_ctg_seqs = read_GFA_ctgs(
         in_GFA_file,
-        attributes_list=[GFA_SEQ_KEY],
+        attributes_list=['all'],
         gzipped=in_gzipped
     )
     ctg_records = [
-        SeqRecord(Seq(y[GFA_SEQ_KEY]), id=x, name=x, description=f'{x}.GFA')
+        SeqRecord(
+            Seq(y[GFA_SEQ_KEY]),
+            id=x,
+            name=x,
+            description=f'{x}.GFA {__write_attributes(y, keys_to_remove=[GFA_SEQ_KEY])}'
+        )
         for x,y in GFA_ctg_seqs.items()
     ]
     with __open_file_write(out_FASTA_file, gzipped=out_gzipped) as out_file:
