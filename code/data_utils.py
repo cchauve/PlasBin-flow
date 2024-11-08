03c2b180a83f94a49fed80ab00b423612ae73832
Computational Methods for  Comparative Genomics at Simon Fraser University <cedric.chauve@sfu.ca>
2024-11-08 14:57:24 -0800

    Update data_utils.py
    Reading seeds file

--- code/data_utils.py
+++ code/data_utils.py
@@ -136,26 +136,37 @@ def read_ctgs_data(
             ctg_data[SCORE_KEY] = default_pls_score
 
     return ctgs_data_dict
-    
+
+def read_seeds_file(in_seeds_file):
+    """
+    Reads the set of seed contigs from a file
+    Args:
+        - in_seeds_file (str/None): path to seeds file if it exists
+    Returns:
+        Set(contig names)
+    """
+    seeds = Set()
+     with open(in_seeds_file) as in_file:
+        for seed_line in in_file.readlines():
+            ctg_id = seed_line.rstrip()
+            seeds.add(ctg_id)
+    return seeds
+
 def get_seeds(ctgs_data_dict, seeds_file, seed_len, seed_score):
     """
     Computes the set of seed contigs and updates the contigs data
     Args:
         - ctgs_data_dict (Dictionary): see above
-        - seeds_file (str/None): pah to seeds file if it exists
+        - seeds_file (str/None): path to seeds file if it exists
         - seed_len (int): length threshold defining seeds
         - seed_score (float): plasmid score threshold defining seeds
     Returns:
         Set(contig names)
         Updates the fild SEED_KEY of ctgs_data_dict
     """
-    seeds = set()
-    ctgs_id_list = list(ctgs_data_dict.keys())
     if seeds_file is not None:
-        with open(in_seeds_file) as in_file:
-            for seed_line in in_file.readlines():
-                ctg_id = seed_line.rstrip()
-                seeds.add(ctg_id)
+        seeds = read_seeds_file(seeds_file)
+        ctgs_id_list = list(ctgs_data_dict.keys())
         check_lists_inclusion(
             list(seeds),
             ctgs_id_list,
@@ -164,6 +175,7 @@ def get_seeds(ctgs_data_dict, seeds_file, seed_len, seed_score):
         for ctg_id in seeds:
             ctg_data[ctg_id][SEED_KEY] = True
     else:
+        seeds = Set()
         for ctg_id,ctg_data in ctgs_data_dict.items():
             len_test = ctg_data[LEN_KEY] >= seed_len            
             score_test = ctg_data[SCORE_KEY] >= seed_score
